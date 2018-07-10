a760e4b46a532c263a93c7240e1fb092739c7f31
pprimdore <ppridmore@sosacorp.com>
2018-07-10 14:51:25 -0700

    add push for files

--- mapbuild/push.sh
+++ mapbuild/push.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
 
-commit_files() {
-  git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git push
-}
+git add .
+git commit --message "Travis build"
+git push origin HEAD:development
 
-setup_git
-commit_files
+
