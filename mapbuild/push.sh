856d410452329e2b064e80ea1ba49c6edb617f10
pprimdore <ppridmore@sosacorp.com>
2018-07-10 15:23:10 -0700

    Travis build

--- mapbuild/push.sh
+++ mapbuild/push.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 cd ../
-git checkout -b development
 git add .
 git commit --message "Travis build"
 git remote add . https://${GITHUB_TOKEN}@github.com/DigitalLifeCollective/mapped-projects
