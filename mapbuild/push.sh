d85a35927f669071e90f47cf5c375df013b5ee6a
pprimdore <ppridmore@sosacorp.com>
2018-07-10 16:08:56 -0700

    fixed file paths

--- mapbuild/push.sh
+++ mapbuild/push.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
-git config user.email "travis@travis.org"
-git config user.name "Travis CI"
 
-git add .
-git commit --message "Travis build"
-git push -q https://${GITHUB_TOKEN}@github.com/DigitalLifeCollective/mapped-projects.git development
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
+commit_files() {
+  git checkout -b development
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
 
+upload_files() {
+ git push  https://${GITHUB_TOKEN}@github.com/DigitalLifeCollective/mapped-projects.git development
+}
+
+setup_git
+commit_files
+upload_files
