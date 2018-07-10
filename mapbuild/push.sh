32abe75ce4a896128d2d4542df1ccddbec07d5fd
pprimdore <ppridmore@sosacorp.com>
2018-07-10 15:12:40 -0700

    add push for files

--- mapbuild/push.sh
+++ mapbuild/push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-git add .
+git checkout -b development
+git remote add . https://${GITHUB_TOKEN}@github.com/DigitalLifeCollective/mapped-projects > /dev/null 2>&1
 git commit --message "Travis build"
 git push origin HEAD:development
 
