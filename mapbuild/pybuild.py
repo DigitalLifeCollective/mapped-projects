8611513aef51eceec02cc973f099770af5c418a7
pprimdore <ppridmore@sosacorp.com>
2018-07-10 18:40:13 -0700

    fixed file paths

--- mapbuild/pybuild.py
+++ mapbuild/pybuild.py
@@ -97,7 +97,7 @@ a = getmap(map_id)
 print(a)
 
 
-command = 'git remote -v'
+command = 'git checkout -b build-branch'
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
 print(output)
