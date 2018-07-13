bb78f32219f706be2eaee305386c30fb94c0a75a
pprimdore <ppridmore@sosacorp.com>
2018-07-13 10:21:42 -0700

    Updated markdown format

--- push.sh
+++ push.sh
@@ -15,9 +15,13 @@ commit_files() {
 
 upload_files() {
 
+
+  git remote add origin-pages https://${GH_TOKEN}@github.com/DigitalLifeCollective/mapped-projects/.git > /dev/null 2>&1
   git push --quiet --set-upstream origin HEAD travis-build:$TRAVIS_BUILD_NUMBER
 }
 
 setup_git
 commit_files
 upload_files
+
+
