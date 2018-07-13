3123ee8063b54149165aa79489fd669932e3c117
Travis CI <travis@travis-ci.org>
2018-07-13 10:42:42 -0700

    Travis build: 0001 [skip ci]

--- push.sh
+++ push.sh
@@ -8,7 +8,7 @@ setup_git() {
 }
 
 commit_files() {
-  git checkout -b travis-build:$TRAVIS_BUILD_NUMBER
+  git checkout -b travis-build-$TRAVIS_BUILD_NUMBER
   git add --all .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 }
@@ -17,7 +17,7 @@ upload_files() {
 
 
   git remote add origin-pages https://${GH_TOKEN}@github.com/DigitalLifeCollective/mapped-projects/.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin HEAD:travis-build:$TRAVIS_BUILD_NUMBER --force
+  git push --quiet --set-upstream origin HEAD:travis-build-$TRAVIS_BUILD_NUMBER --force
 }
 
 setup_git
