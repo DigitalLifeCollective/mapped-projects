a8c34118e93ad352eee4d759b04b36bbd006cdb6
Travis CI <travis@travis-ci.org>
2018-07-13 10:29:28 -0700

    Travis build: 0001 [skip ci]

--- push.sh
+++ push.sh
@@ -17,7 +17,7 @@ upload_files() {
 
 
   git remote add origin-pages https://${GH_TOKEN}@github.com/DigitalLifeCollective/mapped-projects/.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin HEAD travis-build:$TRAVIS_BUILD_NUMBER
+  git push --quiet --set-upstream origin HEAD travis-build:$TRAVIS_BUILD_NUMBER -force
 }
 
 setup_git
